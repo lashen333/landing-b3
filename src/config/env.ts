47bbfbd941e3c282e21579913ecd8c918bf90ad3
Sanju Peramuna <sanju.peramuna@gmail.com>
2025-09-28 11:09:15 +0530

    hiss

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,6 +5,7 @@ export const env = {
     PORT:Number(process.env.PORT ?? 4000),
     MONGODB_URI:process.env.MONGODB_URI ?? "",
     FRONTEND_ORIGIN:process.env.FRONTEND_ORIGIN ?? "*",
+    DOMAIN: process.env.DOMAIN ?? "http://localhost", 
 };
 if(!env.MONGODB_URI){
     console.warn("MONGODB_URI is not set.Add it to your .env");
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 // src\server.ts
 import app from "./app";
-import {connectDB} from "./config/db";
-import {env} from "./config/env";
+import { connectDB } from "./config/db";
+import { env } from "./config/env";
 
-async function main(){
-    await connectDB();
-    app.listen(env.PORT,()=>{
-        console.log(`API listening on http://localhost:${env.PORT} `)
-    });
+async function main() {
+  await connectDB();
+  app.listen(env.PORT, "0.0.0.0", () => {
+  console.log(`API listening at ${env.DOMAIN}`);
+});
 }
-main().catch((err) =>{
-    console.error("Failed to start server:",err);
-    process.exit(1);
+main().catch((err) => {
+  console.error("Failed to start server:", err);
+  process.exit(1);
 });
